f81e33da795d58c6f0d3d459a262820758b0c2ca
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 10:44:08 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,15 +1,15 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
 let package = Package(
     name: "swift-clocks",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27"),
-        .visionOS("27")
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
+        .visionOS(.v27),
     ],
     products: [
         .library(
@@ -18,19 +18,19 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-foundations/swift-kernel.git", branch: "main"),
+        .package(url: "https://github.com/swift-foundations/swift-kernel.git", branch: "main")
     ],
     targets: [
         .target(
             name: "Clocks",
             dependencies: [
-                .product(name: "Kernel Clock", package: "swift-kernel"),
+                .product(name: "Kernel Clock", package: "swift-kernel")
             ]
         ),
         .testTarget(
             name: "Clocks Tests",
             dependencies: [
-                "Clocks",
+                "Clocks"
             ]
         ),
     ],
